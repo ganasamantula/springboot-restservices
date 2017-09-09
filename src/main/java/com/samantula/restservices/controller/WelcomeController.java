1a0c3b082fe0d234996b0302731023a001c204bb
ganasamantula <infosamantula@gmail.com>
2017-09-09 14:48:54 -0700

    Second  commit

---
+++ src/main/java/com/samantula/restservices/controller/WelcomeController.java
@@ -0,0 +1,34 @@
+package com.samantula.restservices.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+class WelcomeController {
+	
+//    @SpringBean
+//	private WelcomeService welcome;
+//    
+//    
+//    @SpringBean
+//	private NisumUserService nisumService;
+//
+//
+//	@GetMapping("/surveys")
+//	public String welcomeMsg() {
+//    	System.out.println("*******************&&&&&&&&&&&&&&&&&&&&" );
+//    	System.out.println("*******************" + welcome.toString());
+//		return welcome.retrieveWelcomeMessage();
+//	}
+//    
+//	
+//	@GetMapping("/surveys/users")
+//	public List<User> retrieveUsers() {
+//		
+//		System.out.println("*******************" + nisumService.toString());
+//		System.out.println("*******************" + nisumService.getUsers());
+//		return nisumService.getUsers();
+//	}
+
+}
